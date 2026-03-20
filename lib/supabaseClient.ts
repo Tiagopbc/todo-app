9e365334c828de623e4f986e2a6fb3484731f473
tiagopbc <pereirabc1981@gmail.com>
2026-03-20 20:34:36 -0300

    fix: resolve vercel build errors

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 // 👇 Atenção para o nome da variável atualizado aqui!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
